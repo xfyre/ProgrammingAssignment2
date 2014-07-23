3e971381c41ec49356132a874797b47ced37405e
Ilya Obshadko <xfyre@xfyre.com>
2014-07-23 13:35:19 +1100

    remove testing code

--- cachematrix.R
+++ cachematrix.R
@@ -35,38 +35,3 @@ cacheSolve <- function(x, ...) {
   
   inverse
 }
-
-### testing the above functions
-
-message("starting tests")
-message("creating 1000 x 1000 matrix")
-testmatrix <- replicate(1000, rnorm(1000))
-
-message("solving matrix")
-testinverse <- solve(testmatrix)
-
-message("caching matrix")
-cachedmatrix <- makeCacheMatrix(testmatrix)
-
-message("solving cached matrix 1st time")
-start <- Sys.time()
-inverse <- cacheSolve(cachedmatrix)
-end <- Sys.time()
-message (paste("execution took", end - start, "seconds"))
-
-message("solving cached matrix 2nd time")
-start <- Sys.time()
-inverse <- cacheSolve(cachedmatrix)
-end <- Sys.time()
-message (paste("execution took", end - start, "seconds"))
-
-message("comparing cached solution with non-cached")
-if (isTRUE(all.equal(testinverse, inverse))) {
-  message("matrices are equal, passed successfully")
-} else {
-  error("matrix equality test failed")
-}
-
-message("tests completed")
-
-rm(list=ls())
